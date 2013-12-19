da111c4fbc220bc9fe49238074bdc54147a2e809
Minh Nguyen <mnx2012@gmail.com>
2013-12-20 01:14:39 +0700

    set default currentMethod is fitWidth

--- freewall.js
+++ freewall.js
@@ -1010,8 +1010,9 @@
             },
 
             refresh: function() {
-                var args = arguments.length ? arguments : runtime.currentArguments;
-                runtime.currentMethod.apply(this, Array.prototype.slice.call(args, 0));
+                var params = arguments.length ? arguments : runtime.currentArguments;
+                runtime.currentMethod == null && (runtime.currentMethod = this.fitWidth);
+                runtime.currentMethod.apply(this, Array.prototype.slice.call(params, 0));
                 return this;
             },
 
